ee18f37b26a65a89c1f745833237d60ccf78b3ee
Sixiermu <stumaolin_luo@163.com>
2022-07-08 23:16:35 +0800

    --利用spring创建了对象并调用方法

---
+++ src/com/atguigu/spring5/testdemo/TestSpring.java
@@ -0,0 +1,20 @@
+package com.atguigu.spring5.testdemo;
+import com.atguigu.spring5.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring{
+    @Test
+    public void testAdd(){
+        //加载spring配置文件
+        ApplicationContext context=
+                new ClassPathXmlApplicationContext("bean1.xml");
+
+        //获取配置创建的对象 .class 表示转换类型
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        user.add();
+    }
+}
+

dcc448f82d084abfcdb474b598c4920742bbd93d
Sixiermu <stumaolin_luo@163.com>
2022-07-13 22:20:19 +0800

    20220713 注入属性 外部bean

---
+++ src/com/atguigu/spring5/bean/Emp.java
@@ -0,0 +1,14 @@
+package com.atguigu.spring5.bean;
+
+public class Emp {
+    private String ename;
+    private String gender;
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}

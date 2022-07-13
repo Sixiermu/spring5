254ee6dce49b1b0e957eecd9991a7fea66efa511
Sixiermu <stumaolin_luo@163.com>
2022-07-13 22:02:17 +0800

    2022-7-13 lml spring Bean管理-属性注入

--- src/com/atguigu/spring5/User.java
+++ src/com/atguigu/spring5/User.java
@@ -1,7 +1,33 @@
 package com.atguigu.spring5;
 
 public class User {
+    private String name;
+    private String bauhtor;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBauhtor() {
+        return bauhtor;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBauhtor(String bauhtor) {
+        this.bauhtor = bauhtor;
+    }
+
     public  void add(){
         System.out.println("add...");
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("2");
     }
 }
--- src/com/atguigu/spring5/testdemo/TestSpring.java
+++ src/com/atguigu/spring5/testdemo/TestSpring.java
@@ -1,20 +1,26 @@
 package com.atguigu.spring5.testdemo;
+import com.atguigu.spring5.Book;
+import com.atguigu.spring5.Order;
 import com.atguigu.spring5.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class TestSpring{
     @Test
     public void testAdd(){
-        //加载spring配置文件
+/*        //加载spring配置文件
         ApplicationContext context=
                 new ClassPathXmlApplicationContext("bean1.xml");
 
         //获取配置创建的对象 .class 表示转换类型
-        User user = context.getBean("user", User.class);
-        System.out.println(user);
-        user.add();
+        User user = context.getBean("user", User.class);*/
+        ApplicationContext context = new FileSystemXmlApplicationContext
+                ("D:\\project\\spring5_demo1\\src\\bean1.xml");
+        //类似反射
+        Book order = context.getBean("book",Book.class);
+        order.bookTest();
     }
 }
 

c79c7891e638ac840ec981ecd64fe7db6f33b2fb
Sixiermu <stumaolin_luo@163.com>
2022-07-14 22:14:27 +0800

    2022/7/14 集合类型注入 Signed-off-by: Sixiermu

---
+++ src/com/atguigu/spring5/colletiontype/Stu.java
@@ -0,0 +1,38 @@
+package com.atguigu.spring5.colletiontype;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Stu {
+    //数组
+    private  String[] courses;
+    //list集合
+    private List<String> list;
+    //Map
+    private Map<String,String> map;
+    //set
+    private Set<String> set;
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+    public  void test(){
+        System.out.println(Arrays.toString(courses));
+        System.out.println(list);
+        System.out.println(map);
+        System.out.println(set);
+    }
+}

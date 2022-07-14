5862bbe44187788284429401f0f40cee5d8da4e7
Sixiermu <stumaolin_luo@163.com>
2022-07-14 20:40:23 +0800

    2022.7.14 内部bean的使用

--- src/com/atguigu/spring5/bean/Dept.java
+++ src/com/atguigu/spring5/bean/Dept.java
@@ -1,9 +1,23 @@
 package com.atguigu.spring5.bean;
 
 public class Dept {
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
     private  String dname;
+
+    public Dept() {
+    }
 
     public Dept(String dname) {
         this.dname = dname;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "dname='" + dname + '\'' +
+                '}';
     }
 }
--- src/com/atguigu/spring5/bean/Emp.java
+++ src/com/atguigu/spring5/bean/Emp.java
@@ -4,11 +4,20 @@ public class Emp {
     private String ename;
     private String gender;
 
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+
+    //员工属于某个部门 用对象表示
+    private  Dept dept;
     public void setEname(String ename) {
         this.ename = ename;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+    public void add(){
+        System.out.println(ename + "::" + gender +"::" + dept);
     }
 }

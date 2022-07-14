1574dc9757f01d262e60919354c3e2378983613c
Sixiermu <stumaolin_luo@163.com>
2022-07-14 22:12:39 +0800

    2022/7/14 TestBean 重命名 TestBean1_5

--- src/com/atguigu/spring5/testdemo/TestBean1_5.java
+++ src/com/atguigu/spring5/testdemo/TestBean1_5.java
@@ -2,13 +2,14 @@ package com.atguigu.spring5.testdemo;
 import com.atguigu.spring5.Book;
 import com.atguigu.spring5.User;
 import com.atguigu.spring5.bean.Emp;
+import com.atguigu.spring5.colletiontype.Stu;
 import com.atguigu.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-public class TestBean {
+public class TestBean1_5 {
     @Test
     public void testBean2(){
 
@@ -40,6 +41,17 @@ public class TestBean {
         //获取配置文件创建的对象.class 表示转换类型
         Emp emp = context.getBean("emp", Emp.class);
         emp.add();
+    }
+    @Test
+    public void testBean5(){
+
+        //加载配置文件 src下相对路径
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean5.xml");
+
+        //获取配置文件创建的对象.class 表示转换类型
+
+        Stu stu = context.getBean("stu", Stu.class);
+        stu.test();
     }
 }
 
